38963a94b61d39ee2cc4dbe6dd11ad89d4ab9c1c
Zakhar Borisov <zakhar.borisov@mail.ru>
2022-12-08 14:52:11 +0900

    init commit

---
+++ src/test/java/ru/borisov/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.borisov.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
